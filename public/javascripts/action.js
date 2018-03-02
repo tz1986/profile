33891a800de167f34815668152fa682ef8f9369c
Jack Huang <jackhuang@Jacks-MBP.local>
2018-03-01 17:14:18 -0800

    init

---
+++ public/javascripts/action.js
@@ -0,0 +1,86 @@
+// function selectHome() {
+//     $('.jack-nav a.active').removeClass('active');
+//     $('#home-tag').addClass('active');
+// }
+//
+// function selectMessage() {
+//     $('.jack-nav a.active').removeClass('active');
+//     $('#message-tag').addClass('active');
+// }
+//
+// function selectFriends() {
+//     $('.jack-nav a.active').removeClass('active');
+//     $('#friends-tag').addClass('active');
+// }
+
+
+/*function selectNavButton(buttonID) {
+    $('.jack-nav a.active').removeClass('active');
+    $('#'+buttonID).addClass('active');
+}*/
+
+function selectNavButton(buttonID, targetID) {
+    $('.jack-nav a.active').removeClass('active')
+    $('#' + buttonID).addClass('active');
+    $('html, body').animate({
+        scrollTop: $('#' + targetID).offset().top
+    }, 400);
+    //alert(buttonID + '\n' + targetID);
+}
+
+
+function selectMenuButton(buttonID) {
+    $('.jack-menu a.active').removeClass('active')
+    $('#' + buttonID).addClass('active');
+   /* $('.jack-menu').transition('fade up');*/
+    if (buttonID === 'app-tag') {
+        for (var i = 0; i < 6; i++) {
+            $('#work-img-' + (i + 1)).attr("src", "/images/app-img0" + (i + 1) + ".jpg");
+        }
+    }
+    if (buttonID === 'website-tag') {
+        for (var i = 0; i < 6; i++) {
+            $('#work-img-' + (i + 1)).attr("src", "/images/pic0" + (i + 1) + ".jpg");
+        }
+    }
+    if (buttonID === 'graphic-tag') {
+        for (var i = 0; i < 6; i++) {
+            $('#work-img-' + (i + 1)).attr("src", "/images/pic0" + (i + 1) + ".jpg");
+        }
+
+    }
+    if (buttonID === 'photography-tag') {
+        for (var i = 0; i < 6; i++) {
+            $('#work-img-' + (i + 1)).attr("src", "/images/app-img0" + (i + 1) + ".jpg");
+        }
+
+    }
+    if (buttonID === 'arts-tag') {
+        for (var i = 0; i < 6; i++) {
+            $('#work-img-' + (i + 1)).attr("src", "/images/pic0" + (i + 1) + ".jpg");
+        }
+
+    }
+
+}
+
+
+function selectWorkImage(imgTag) {
+    //$('#'+imgTag);
+    //alert('JACK IN THE BOX');
+    var holder_width = $('div#img-detail-holder').width();
+
+    $('#img-detail').css('max-width', (holder_width-40)+'px');
+    //$('#img-detail').css('margin', '0px');
+    //var img_width = $('#img-detail').width()
+    //alert(img_width);
+    $('div#img-detail-holder').modal('show');
+
+}
+
+// function didHitGetStarted() {
+//     $('js-gtco-nav-toggle').removeClass('active');
+//     $('html，body').animate({
+//         scrollTop: $('#jk-contact').offset().top
+//     }, 1000);
+// }
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('home', {
+        title: 'Jack Huang'});
+});
+
+//做页功能//
+/*router.use('/home/:tag', function (req, res, next) {
+   console.log(req.params.tag);
+    res.render('home', {
+        title: "JACK - IN THE BOX" + req.params.tag
+    });*/
+// });
+
+module.exports = router;
