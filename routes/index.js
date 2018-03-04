2139438d282d0f08172b46fa0a5be1eada04b8d5
Jack <htz103004176@gmail.com>
2018-03-04 15:14:45 -0800

    image responsive

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function (req, res, next) {
     res.render('home', {
-        title: 'Jack Huang'});
+        title: 'Jack Huang',
+        workStatus: 'app'
+    });
 });
+
+router.use('/:workStatus', function (req, res, next) {
+    console.log(req.params.workStatus);
+    res.render('home', {
+        title: 'Jack Huang',
+        workStatus: req.params.workStatus
+    });
+});
+
+
 
 //做页功能//
 /*router.use('/home/:tag', function (req, res, next) {
